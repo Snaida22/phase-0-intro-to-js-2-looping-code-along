30e1dbf9ecd53f10a6ac54a8f6e18d3b68676a0c
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-04 14:53:34 +0300

    commit tree 3e11a8c334b1a57b599a4a8e0834b36a442bf2e8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// returns an array of thank you messages for each name provided to the function
+function writeCards(name){
+    let message=[]
+    for ( let i=0; i<name.length; i++ ){
+        message.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
+// invokes console.log once for each number, counting down from the number provided to zero
+function countDown(name){
+    
+    while ( name> 0){
+        console.log(name)
+        name--;
+    }
+    // logs each number when counting down, starting from the number provided
+    console.log(name)
+}
